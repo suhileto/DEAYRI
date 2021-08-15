3ae029850cef6b613bef2d88d22cac85b0d190d1
talhaucarr <talhaucarr@hotmail.com>
2021-08-15 18:40:03 +0300

    Project initializing

---
+++ Assets/Scripts/PythonExample.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using IronPython.Hosting;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class PythonExample : MonoBehaviour
+{
+	[SerializeField]
+	private Text greeting, randomNumber;
+
+	// Use this for initialization
+	void Start ()
+	{
+		var engine = Python.CreateEngine ();
+
+		ICollection<string> searchPaths = engine.GetSearchPaths ();
+
+		
+		//Path to the folder of test.py
+		searchPaths.Add (Application.dataPath);
+		//Path to the Python standard library
+		searchPaths.Add (Application.dataPath + @"\StreamingAssets"  + @"\Lib\");
+		engine.SetSearchPaths (searchPaths);
+
+		dynamic py = engine.ExecuteFile (Application.dataPath + @"\StreamingAssets" + @"\Python\test.py");
+		dynamic test = py.Test ("Codemaker");
+		greeting.text = "Greeting: " + test.display ();
+		randomNumber.text = "Random Number: " + test.random_number (1, 5);
+		
+
+	}
+
+}
