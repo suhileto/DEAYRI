0abe539766ec993427a44bb709aec0bf13396458
Süha KÜÇÜKALİ <suhakucukali@marun.edu.tr>
2021-08-22 15:23:33 +0300

    Resampling added
    Encoding WAV files for Google Cloud Speech to Text API

--- Assets/Scripts/SpeecToText.cs
+++ Assets/Scripts/SpeecToText.cs
@@ -29,8 +29,19 @@ public class SpeecToText : MonoBehaviour
         }
         #endregion*/
 
-
-        System.Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", "C:/Users/talla/Documents/GitHub/DEAYRI/solid-mountain-323117-23c33e1c114c.json");
+	int outRate = 16000;
+	var inFile = @"dump.wav";
+	var outFile = @"resampled.wav";
+	using (var reader = new WaveFileReader(inFile))
+	{
+		var outFormat = new WaveFormat(outRate, 1);
+		using (var resampler = new MediaFoundationResampler(reader, outFormat))
+			{
+			    WaveFileWriter.CreateWaveFile(outFile, resampler);
+			}
+	}
+		
+        System.Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", "/home/kali/Documents/GitHub/DEAYRI/solid-mountain-323117-23c33e1c114c.json");
         var speech = SpeechClient.Create();
         var response = speech.Recognize(new RecognitionConfig()
         {
@@ -38,9 +49,10 @@ public class SpeecToText : MonoBehaviour
             SampleRateHertz = 48000,
             AudioChannelCount = 2,
             LanguageCode = "tr-TR",
-        }, RecognitionAudio.FromFile("C:/Users/talla/Documents/GitHub/DEAYRI/dump.wav"));
+        }, RecognitionAudio.FromFile("/home/kali/Documents/GitHub/DEAYRI/dump.wav"));
 
-
+	
+	
         string Text = "";
 
         foreach (var result in response.Results)
